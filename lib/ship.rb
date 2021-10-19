06566bbefb0a0758bf74fa6f643781fc45a331d8
Paul Leonard <paulpaulleonard@gmail.com>
2021-10-18 22:45:20 -0400

    Refactor ship class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,7 +1,9 @@
 class Ship
-  attr_accessor :name, :length, :health
+  attr_reader :name,
+              :length,
+              :health
   def initialize (name, length)
-    @name = name
+    @name   = name
     @length = length
     @health = 3
   end
