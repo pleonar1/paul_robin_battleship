33c252741babf294a00745295157f83325ceab1e
Robin Dittrich <robinodittrich@gmail.com>
2021-10-20 17:24:54 -0700

    Create consecutive coordinate test.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -25,14 +25,24 @@ RSpec.describe "Board" do
     expect(board.valid_coordinate?("A22")).to eq false
   end
 
-  it 'can determine valid ship placements' do
+  xit 'can determine valid ship placements' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
-require "pry"; binding.pry
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  it 'makes sure the coordinates are consecutive' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to be(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to be(false)
   end
 end
