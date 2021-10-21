db2ca412b11979800e0163ba7eecc76b3d3e996c
Robin Dittrich <robinodittrich@gmail.com>
2021-10-20 17:44:47 -0700

    Complete test for placing a ship.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -63,4 +63,22 @@ RSpec.describe "Board" do
     expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
+
+  #PLACING SHIPS
+
+  xit 'places a ship' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+
+    expect(cell_1.ship).to eq(cruiser)
+    expect(cell_2.ship).to eq(cruiser)
+    expect(cell_3.ship).to eq(cruiser)
+    expect(cell_3.ship == cell_2.ship).to eq(true)
+  end
 end
