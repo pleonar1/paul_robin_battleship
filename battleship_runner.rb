1b7dabb8bc5eeb9f49fd46b7359c0e757f06a8e2
Robin Dittrich <robinodittrich@gmail.com>
2021-10-23 15:10:44 -0700

    Complete main_menu method and create play method

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,8 +1,10 @@
 require './lib/ship'
-require './lib/card'
+require './lib/cell'
 require './lib/board'
 
-board = Board.new
+@board =      Board.new
+@cruiser =    Ship.new("Cruiser", 3)
+@submarine =  Ship.new("Submarine", 2)
 
 def main_menu
   p "Welcome to BATTLESHIP"
@@ -24,3 +26,31 @@ end
 def start
   p "I have laid out my ships on the grid."
   p "You now need to lay out your two ships."
+  p "The Cruiser is three units long and the Submarine is two units long."
+  @board.render
+  until valid_placement?(@submarine, player_placements) == true
+    player_placements = []
+    p "Enter the squares for the Cruiser (3 spaces):"
+    "Position 1: "
+    '>'
+    cruiser_placement_1 = gets.chomp.to_s
+    player_placements << cruiser_placement_1
+    "Position 2: "
+    '>'
+    cruiser_placement_2 = gets.chomp.to_s
+    player_placements << cruiser_placement_2
+    "Position 3: "
+    '>'
+    cruiser_placement_3 = gets.chomp.to_s
+    player_placements << cruiser_placement_3
+
+    @board.valid_placement?(@submarine, player_placements)
+    if valid_placement? == true
+      puts "Nice! Your cruiser has been placed!"
+    else
+      puts "Invalid input. Please try again."
+    end
+  end
+end
+
+main_menu
