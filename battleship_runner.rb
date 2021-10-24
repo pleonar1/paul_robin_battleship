e4884ea98909b56c6dc48670e1476b96f208c86f
Paul Leonard <paulpaulleonard@gmail.com>
2021-10-24 19:00:19 -0400

    Computer can choose its placements

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -2,9 +2,12 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 
-@board =      Board.new
-@cruiser =    Ship.new("Cruiser", 3)
-@submarine =  Ship.new("Submarine", 2)
+@board        = Board.new
+@comp_board   = Board.new
+@cruiser      = Ship.new("Cruiser", 3)
+@submarine    = Ship.new("Submarine", 2)
+@comp_cruiser = Ship.new("Cruiser", 3)
+@comp_sub     = Ship.new("Submarine", 2)
 
 def main_menu
   p "Welcome to BATTLESHIP"
@@ -27,6 +30,7 @@ def start
   puts "I have laid out my ships on the grid."
   puts "You now need to lay out your two ships."
   puts "The Cruiser is three units long and the Submarine is two units long."
+  puts "==============PLAYER BOARD=============="
   puts @board.render
 
 
@@ -60,6 +64,7 @@ end
 def set_sub
   puts "Now you can set your Submarine"
   puts "For a valid placement, the sub cannot overlap with cruiser"
+  puts "==============PLAYER BOARD=============="
   puts @board.render(true)
 
   player_placements = []
@@ -75,18 +80,66 @@ def set_sub
   if @board.valid_placement?(@submarine, player_placements) == true
     puts "Nice! Your cruiser has been placed!"
     @board.place(@submarine, player_placements)
+    puts "=============COMPUTER BOARD============="
+    puts @comp_board.render
+    puts "==============PLAYER BOARD=============="
     puts @board.render(true)
-    play_the_game
+    set_computer_cruiser
   else
     puts "Invalid input. Please try again."
     set_sub
   end
 end
 
-def play_the_game
-  puts "time to play the game!"
+def set_computer_cruiser
+  computer_cruiser_placements = []
+
+  comp_cruiser_placement_1 = (('A'..'D').to_a.sample + rand(1..4).to_s)
+  computer_cruiser_placements << comp_cruiser_placement_1
+
+  comp_cruiser_placement_2 = (('A'..'D').to_a.sample + rand(1..4).to_s)
+  computer_cruiser_placements << comp_cruiser_placement_2
+
+  comp_cruiser_placement_3 = (('C'..'D').to_a.sample + rand(1..4).to_s)
+  computer_cruiser_placements << comp_cruiser_placement_3
+
+  # puts computer_cruiser_placements
+  if @comp_board.valid_placement?(@comp_cruiser, computer_cruiser_placements) == true
+    @comp_board.place(@comp_cruiser, computer_cruiser_placements)
+    puts computer_cruiser_placements
+    puts @comp_board.render(true) # remove later
+    set_computer_submarine
+
+  else
+    set_computer_cruiser
+  end
+end
+
+def set_computer_submarine
+  computer_sub_placements = []
+
+  comp_sub_placement_1 = (('A'..'D').to_a.sample + rand(1..4).to_s)
+  computer_sub_placements << comp_sub_placement_1
+
+  comp_sub_placement_2 = (('A'..'D').to_a.sample + rand(1..4).to_s)
+  computer_sub_placements << comp_sub_placement_2
+
+  if @comp_board.valid_placement?(@comp_sub, computer_sub_placements) == true
+    @comp_board.place(@comp_sub, computer_sub_placements)
+    puts computer_sub_placements
+    puts @comp_board.render(true) # remove later
+    start_the_match
+  else
+    set_computer_submarine
+  end
+
+end
+
+def start_the_match
+  puts "looks like it worked"
 end
 
 
 
-main_menu
+# main_menu
+set_computer_cruiser
